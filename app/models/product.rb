4f985d8a87bae5b1582898376d856f5da15ca520
lsclike <lsclike2016@hotmail.com>
2017-11-30 10:56:45 +1100

    before Store

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,8 @@ class Product < ApplicationRecord
   scope :search, lambda {|query| where(["title LIKE ?","%#{query}%" ])}
   validates :title, :description, :image_url, presence: true
 
+  validates :title, length: {minimux: 2, maximum: 10}
+
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
 
   validates :title, uniqueness: true
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,15 +1,10 @@
 require 'test_helper'
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
-  setup do
+  setup do #DRY principle#
     @product = products(:one)
-    @update = {
-        title: 'Test book',
-        description: 'Nothing to talk about',
-        image_url: 'test.jpg',
-        price: 10.25
-    }
   end
+
   test "should get index" do
     get products_url
     assert_response :success
@@ -20,19 +15,34 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should create product" do
+    assert_difference('Product.count') do
+      post products_url, params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title } }
+    end
+
+    assert_redirected_to product_url(Product.last)
+  end
+
+  test "should show product" do
+    get product_url(@product)
+    assert_response :success
+  end
+
   test "should get edit" do
     get edit_product_url(@product)
     assert_response :success
   end
 
-  test "should get show" do
-    get product_url
-    assert_response :success
+  test "should update product" do
+    patch product_url(@product), params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title } }
+    assert_redirected_to product_url(@product)
   end
 
-  test "should get delete" do
-    get delete_product_url(@product)
-    assert_response :success
-  end
+  test "should destroy product" do
+    assert_difference('Product.count', -1) do
+      destroy_product_url(@product)
+    end
 
+    assert_redirected_to products_url
+  end
 end
