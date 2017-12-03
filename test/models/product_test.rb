99d96bf2fc13b5e727f09fa2c5db79459385220a
lsclike <lsclike2016@hotmail.com>
2017-12-03 20:59:13 +1100

    finishing cache

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -60,7 +60,7 @@ class ProductTest < ActiveSupport::TestCase
                           price:       1,
                           image_url:   "fred.gif")
 
-    assert product.valid?
+    assert product.invalid?
     assert_equal ["has already been taken"], product.errors[:title]
   end
 
